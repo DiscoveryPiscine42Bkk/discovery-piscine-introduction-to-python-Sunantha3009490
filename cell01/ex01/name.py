c36586609d302f3eb7a4601afa422c7565c89a5b
Sunantha3009490 <sunantha.bam.1212@gmail.com>
2025-06-17 05:10:41 +0000

    add

---
+++ cell01/ex01/name.py
@@ -0,0 +1,4 @@
+first_name = "Sunantha"
+last_name = "Namoakson"
+whole_namme = first_name + " " + last_name
+print(whole_name)
--- cell01/name.py
+++ cell01/name.py
@@ -1,1 +1,5 @@
-print(sunantha, namoakson))
+first_name = "Sunantha"
+last_name = "Namoakson"
+print(first_name)
+print(last_name)
+
