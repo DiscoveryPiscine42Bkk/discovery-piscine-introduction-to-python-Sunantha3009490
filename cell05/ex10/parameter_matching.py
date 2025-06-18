4e08fabace3e1014b3148422ecf7e6040a6e04b0
Sunantha3009490 <sunantha.bam.1212@gmail.com>
2025-06-18 15:09:05 +0000

    add

---
+++ cell05/ex10/parameter_matching.py
@@ -0,0 +1,9 @@
+import sys
+if len(sys.argv) != 2:
+    print("none")
+else:
+    user_input = input("What was the parameter? ").strip()
+    if user_input == sys.argv[1]:
+        print("Good job!")
+    else:
+        print("Nope, sorry...")
