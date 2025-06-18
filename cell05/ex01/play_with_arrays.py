fd1f5647ce8fc1cac7ef6601f9dcb13771cb59d9
Sunantha3009490 <sunantha.bam.1212@gmail.com>
2025-06-18 07:41:53 +0000

    add

---
+++ cell05/ex01/play_with_arrays.py
@@ -0,0 +1,8 @@
+def main():
+    Original_array =  [2, 8, 9, 48, 8, 22, -12, 2]
+    New_array = [x +2 for x in Original_array]
+    print("Original_array:", Original_array)
+    print("new array:" New_array)
+if __name__ == "__main__":
+    main()
+    
---
+++ cell05/ex02/play_with_arrays.py
@@ -0,0 +1,6 @@
+arr1 = [2, 8, 9, 48, 8, 22, -12, 2]
+arr2 = [10, 11, 50, 10, 24]
+arr1_filtered = [x for x in arr1 if x > 5]
+arr2_filtered = [x for x in arr2 if x > 5]
+print(arr1_filtered)
+print(arr2_filtered)
